4ba62019c82e47260563d1c24a8a8dfb28be246e
Dillon Michlena <144554032+dillp1@users.noreply.github.com>
2026-03-23 13:16:18 -0400

    adds header

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="header-nav fixed left-0 right-0 top-0 z-50 bg-[#11182c]">
+      <div className="mx-auto flex h-20 w-full items-center justify-center">
+        <a className="text-[22px] font-bold text-white">HONR Project</a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
