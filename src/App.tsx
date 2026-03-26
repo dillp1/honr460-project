b58649b8f1c2ef72d12293aa9594dfcc5f2e9fef
Dillon Michlena <144554032+dillp1@users.noreply.github.com>
2026-03-26 14:41:01 -0400

    adds a footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import GoogleMap from "./components/GoogleMap";
 import "./App.css";
 import About from "./components/about";
 import CaseStudy1 from "./components/CaseStudy1";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <div className="flex h-screen flex-col items-center p-4 gap-8 pt-24">
+      <div className="flex h-screen flex-col items-center gap-8 pt-24">
         <Header />
         <GoogleMap />
         <About />
         <CaseStudy1 />
+        <Footer />
       </div>
     </>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer w-full flex flex-col px-37.5 py-4">
+      <p className="text-[24px] font-semibold text-white">HONR Project</p>
+    </footer>
+  );
+}
+
+export default Footer;
