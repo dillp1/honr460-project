d617542b35741b2c1215e51ad9adadff0a202500
Dillon Michlena <144554032+dillp1@users.noreply.github.com>
2026-04-14 15:24:18 -0400

    adds a TODO

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -12,6 +12,7 @@ import { Button } from "./ui/button";
 
 const mapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
+/* TODO Add all the case studies */
 function GoogleMap() {
   const markers = [
     {
