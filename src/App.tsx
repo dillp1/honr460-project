6bc4352f3e533c2f2875000db821b70ca6b0bc77
Dillon Michlena <144554032+dillp1@users.noreply.github.com>
2026-04-19 23:09:37 -0400

    adds survey banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import Header from "./components/header";
 import Hero from "./components/Hero";
 import GoogleMap from "./components/GoogleMap";
@@ -9,12 +10,27 @@ import HarrisonHallCard from "./components/HarrisonHallCard";
 import WindsorDiningCourtCard from "./components/WindsorDiningCourtCard";
 import DudleyLambertusCard from "./components/DudleyLambertusCard";
 import HonorsSouthCard from "./components/HonorsSouth";
+import SurveyBanner from "./components/SurveyBanner";
 
 function App() {
+  const [isSurveyBannerVisible, setIsSurveyBannerVisible] = useState(true);
+  const surveyLink =
+    "https://purdue.ca1.qualtrics.com/jfe/form/SV_2bPmS6UdyaeBPlY";
+
   return (
     <>
-      <div className="flex min-h-screen flex-col items-center gap-8 pt-24">
+      <div
+        className={`flex min-h-screen flex-col items-center gap-8 ${
+          isSurveyBannerVisible ? "pt-40" : "pt-24"
+        }`}
+      >
         <Header />
+        {isSurveyBannerVisible ? (
+          <SurveyBanner
+            href={surveyLink}
+            onClose={() => setIsSurveyBannerVisible(false)}
+          />
+        ) : null}
         <Hero />
         <GoogleMap />
         <HarrisonHallCard />
